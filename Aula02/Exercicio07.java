b6b3d6d7daa74d347dc43b2f5bc1e0d43ce37ff7
Christian Augustus <108826449+Jujubileuu@users.noreply.github.com>
2023-10-23 20:26:09 -0300

    Aula 02
    Exercicio 07 completo.
    ESPARTAAAAAAAAAA

--- Aula02/Exercicio07.java
+++ Aula02/Exercicio07.java
@@ -6,8 +6,20 @@ public class Exercicio07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite seu nome:");
-        String palavra = scanner.nextLine();
-        int numero = Integer.parseInt(scanner.nextLine());
+        while (true) {
+            System.out.println("LEONIDAS: SOLDADO TENTE ADIVINHAR QUANTOS INIMIGOS IREMOS ENFRENTAR " +
+                    "\ndigite um número inteiro: ");
+            int inimigos = Integer.parseInt(scanner.nextLine());
+
+            if (inimigos == 300000) {
+                System.out.println("LEONIDAS: CORRETO SOLDADO! ISSO É ESPARTAAAAA!!! " +
+                        "\n!Você Ganhou!");
+                break;
+            } else if (inimigos < 300000) {
+                System.out.println("LEONIDAS: UM POUCO MAIS SOLDADO\n");
+            } else if (inimigos > 300000) {
+                System.out.println("LEONIDAS: UM POUCO MENOS SOLDADO\n");
+            }
+        }
     }
 }
