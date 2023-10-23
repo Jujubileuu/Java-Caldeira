c98f4d9259ae6a03265e73388d7a73870a03e12e
Christian Augustus <108826449+Jujubileuu@users.noreply.github.com>
2023-10-23 19:12:16 -0300

    Aula01
    Exercicio 13 completo

--- Aula01/Exercicio13.java
+++ Aula01/Exercicio13.java
@@ -6,8 +6,13 @@ public class Exercicio13 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite sua idade: ");
-        int idade = Integer.parseInt(scanner.nextLine());
-        String texto = scanner.nextLine();
+        System.out.println("Digite qualquer número inteiro positivo: ");
+        int numero = Integer.parseInt(scanner.nextLine());
+        int fatorial = numero;
+
+        for (int i = 1; i < numero; i++) {
+            fatorial = fatorial * i;
+        }
+        System.out.println(fatorial);
     }
 }
