8a5404ef5beba30480e4d5dd453f545a742eaccc
Christian Augustus <108826449+Jujubileuu@users.noreply.github.com>
2023-10-23 18:59:53 -0300

    Aula01
    Exercicio 12 completo

--- Aula01/Exercicio12.java
+++ Aula01/Exercicio12.java
@@ -6,8 +6,12 @@ public class Exercicio12 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite sua idade: ");
-        int idade = Integer.parseInt(scanner.nextLine());
-        String texto = scanner.nextLine();
+        System.out.println("Digite um valor em dólares (ponto para os centavos): ");
+        double dolar = Double.parseDouble(scanner.nextLine());
+
+        System.out.println("Digite o câmbio atual da moeda de conversão desejada (ponto para os centavos): ");
+        double cambioAtual = Double.parseDouble(scanner.nextLine());
+
+        System.out.println("O valor convertido para a moeda desejada é: " + (dolar * cambioAtual));
     }
 }
