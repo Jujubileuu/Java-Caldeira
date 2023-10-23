de349e4028d5f7c78308e22f1200b22af35a3e22
Christian Augustus <108826449+Jujubileuu@users.noreply.github.com>
2023-10-23 20:14:07 -0300

    Aula 02
    Exercicio06 completo

---
+++ Aula02/Exercicio07.java
@@ -0,0 +1,13 @@
+package Aula02;
+
+import java.util.Scanner;
+
+public class Exercicio07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome:");
+        String palavra = scanner.nextLine();
+        int numero = Integer.parseInt(scanner.nextLine());
+    }
+}
