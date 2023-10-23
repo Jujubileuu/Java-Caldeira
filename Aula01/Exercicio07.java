029ef9e34ce66a9ba720ec9bd26abbf0d8bad484
Christian Augustus <108826449+Jujubileuu@users.noreply.github.com>
2023-10-23 18:06:01 -0300

    Aula01
    Exercicio 07 terminado

--- Aula01/Exercicio07.java
+++ Aula01/Exercicio07.java
@@ -6,8 +6,16 @@ public class Exercicio07 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println();
-        String texto = scanner.nextLine();
+        System.out.println("Digite sua idade: ");
         int idade = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Digite seu salário, utilizando ponto para os centavos: ");
+        double salario = Double.parseDouble(scanner.nextLine());
+
+        if (idade >= 18 && salario > 2000) {
+            System.out.println("Requisitos mínimos atendidos, compra liberada.");
+        } else {
+            System.out.println("Idade ou salário não batem os requisitos mínimos para compra.");
+        }
     }
 }
