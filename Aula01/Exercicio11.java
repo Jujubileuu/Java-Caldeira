015adb1b0f6a0f823987991de780d7f690f87b5d
Christian Augustus <108826449+Jujubileuu@users.noreply.github.com>
2023-10-23 18:55:06 -0300

    Aula 01
    Exercicio 11 completo

--- Aula01/Exercicio11.java
+++ Aula01/Exercicio11.java
@@ -1,13 +1,29 @@
 package Aula01;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Exercicio11 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite sua idade: ");
-        int idade = Integer.parseInt(scanner.nextLine());
-        String texto = scanner.nextLine();
+        System.out.println("Digite uma palavra ou frase: ");
+        String texto = scanner.nextLine().toLowerCase();
+
+        int vogaisQuant = 0;
+
+        for (int i = 0; i < texto.length(); i++) {
+            if (texto.charAt(i) == 'a') {
+                vogaisQuant++;
+            } else if (texto.charAt(i) == 'e') {
+                vogaisQuant++;
+            } else if (texto.charAt(i) == 'i') {
+                vogaisQuant++;
+            } else if (texto.charAt(i) == 'o') {
+                vogaisQuant++;
+            } else if (texto.charAt(i) == 'u') {
+                vogaisQuant++;
+            }
+        }
+        System.out.println("A quantidade de vogais é: " + vogaisQuant);
     }
 }
